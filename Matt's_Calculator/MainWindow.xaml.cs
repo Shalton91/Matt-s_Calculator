6c2ed77d8f183791b7baf7917d0c21b1f914f5ac
Sean Halton <31735592+Shalton91@users.noreply.github.com>
2022-10-18 11:25:23 -0500

    Initial commit

---
+++ Matt's_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Matt_s_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<TabSettings> Tabs { get; set; } = new List<TabSettings>();
+        public MainWindow()
+        {
+
+
+            InitializeComponent();
+
+            var path = System.IO.Directory.GetCurrentDirectory() + @"\tabsettings.json";
+            Tabs = System.Text.Json.JsonSerializer.Deserialize<List<TabSettings>>(System.IO.File.ReadAllText(path));
+            foreach(TabSettings tab in Tabs)
+            {
+                TabItem tabItem = new TabItem();
+                tabItem.Header = tab.Title;
+                tabItem.Tag = tab;
+                tcMain.Items.Add(tabItem);
+            }
+
+
+        }
+    }
+}
---
+++ Matt's_Calculator/TabSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matt_s_Calculator
+{
+    internal class TabSettings
+    {
+        public string Title { get; set; }
+        public double BackHeight { get; set; }
+        public double BackWidth { get; set; }
+        public double? SideHeight { get; set; }
+        public double? SideWidth { get; set; }
+    }
+}
